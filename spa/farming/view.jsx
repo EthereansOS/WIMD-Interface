3b380027c02962e11cc65290927acbae509f0af9
alet89 <alessandrotoschi@me.com>
2020-10-28 21:53:08 +0100

    push

--- spa/farming/view.jsx
+++ spa/farming/view.jsx
@@ -15,6 +15,30 @@ var Farming = React.createClass({
                     <br></br>
                     <a className="unibtn" href={window.context.uniswapSpawUrlTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;swap</a>
                 </section>)}
+                <section className="FarmingSeason">
+                <h2 className="BrandizedS">Season 1 <a target="_blank" href="https://etherscan.io/block/countdown/11246089">(Ending block n. 11246089)</a></h2>
+                    <section className="FarmingTier">
+                        <img src=""></img>
+                        <p>Earn 1 Name Card for every:</p> 
+                        <section className="FarmingTierDeal">
+                            <img src=""></img>
+                            <p>100 Name Second Token locked in the Uniswap V2 pair Token 1 - Token 2 for "Time" (Available: )</p>
+                            <a className="GoToFarm">FARM</a>
+                        </section>
+                    </section>
+                </section>
+                <section className="FarmingSeason">
+                <h2 className="BrandizedS">Long Term Season <a target="_blank" href="https://etherscan.io/block/countdown/11246089">(Ending block n. 11246089)</a></h2>
+                    <section className="FarmingTier">
+                        <img src=""></img>
+                        <p>Earn 1 Name Card for every:</p> 
+                        <section className="FarmingTierDeal">
+                            <img src=""></img>
+                            <p>100 Name Second Token locked in the Uniswap V2 pair Token 1 - Token 2 for "Time" (Available: )</p>
+                            <a className="GoToFarm">FARM</a>
+                        </section>
+                    </section>
+                </section>
             </section>
         </section>);
     }
