2f35e1b5e6481900f186427b47c37422552a7fe3
alet89 <alessandrotoschi@me.com>
2020-10-28 21:16:58 +0100

    card Farming

--- spa/farming/view.jsx
+++ spa/farming/view.jsx
@@ -7,11 +7,15 @@ var Farming = React.createClass({
     },
     render() {
         return (<section>
-            {this.props.sortedItems.map((it, i)=> <section key={it.key} className="collection">
-                <CardInfo card={it} />
-                {this.state && this.state.items && this.state.items[i] && <span>Price: $ {window.formatMoney(this.state.items[i].priceInDollars, 1)}</span>}
-                <a className="unibtn" href={window.context.uniswapSpawUrlTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;swap</a>
-            </section>)}
+            <section className="FarmingTop">
+                <h2 className="BrandizedS">Cards</h2>
+                {this.props.sortedItems.map((it, i)=> <section key={it.key} className="collection">
+                    <CardInfo card={it} />
+                    {this.state && this.state.items && this.state.items[i] && <span className="UniPriceBro">$ {window.formatMoney(this.state.items[i].priceInDollars, 1)}</span>}
+                    <br></br>
+                    <a className="unibtn" href={window.context.uniswapSpawUrlTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;swap</a>
+                </section>)}
+            </section>
         </section>);
     }
 });
