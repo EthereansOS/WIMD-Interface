2c5cd1509dcdf9e2003069bde7ef400bdbff6ade
Marco Vasapollo <ceo@metaring.com>
2020-10-29 01:32:49 +0100

    Staking Data

--- spa/farming/view.jsx
+++ spa/farming/view.jsx
@@ -20,7 +20,7 @@ var Farming = React.createClass({
             <section className="FarmingTop">
                 <h2 className="BrandizedS">Cards</h2>
                 {this.props.sortedItems.map((it, i) => <section key={it.key} className="collection">
-                    <CardInfo card={it} clean hideBalance/>
+                    <CardInfo card={it} clean hideBalance />
                     {this.state && this.state.items && this.state.items[i] && <span className="UniPriceBro">$ {window.formatMoney(this.state.items[i].priceInDollars, 1)}</span>}
                     <br></br>
                     <a className="unibtn" href={window.context.uniswapSpawUrlTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;swap</a>
@@ -36,18 +36,20 @@ var Farming = React.createClass({
                         {Object.entries(season).filter(it => it[1].stakingData && it[1].stakingData.stakingData && it[1].stakingData.stakingData.length > 0).map(tokenEntry => {
                             var tokenKey = tokenEntry[0];
                             var tokenValue = tokenEntry[1];
-                            var stakingData = tokenValue.stakingData.stakingData[0];
-                            var amountFor1 = stakingData.tiers[0].percentage;
-                            return (<section key={tokenKey} className="FarmingTier">
-                                <img src={window.formatLink(stakingData.rewardToken.logo || stakingData.rewardToken.logoURI)}/>
-                                <p>Earn 1 {stakingData.rewardToken.name} Card for every:</p>
-                                <a className="GoToFarm" target="_blank" href={window.context.stakingUrlTemplate.format(stakingData.stakingManager.options.address)}>FARM</a>
-                                <br></br>
-                                <section className="FarmingTierDeal">
-                                    <img src={window.formatLink(stakingData.mainToken.logo || stakingData.mainToken.logoURI)}/>
-                                    <p>{window.formatMoney(amountFor1, 2)} {stakingData.mainToken.symbol} locked in the Uniswap V2 pair {stakingData.mainToken.symbol} - {stakingData.pairs[0].symbol} {stakingData.tiers[0].tierKey} (Available: {window.fromDecimals(stakingData.tiers[0].remainingToStake, stakingData.mainToken.decimals)})</p>
-                                </section>
-                            </section>);
+                            var stakingDatas = tokenValue.stakingData.stakingData;
+                            return stakingDatas.map(stakingData => {
+                                var amountFor1 = stakingData.tiers[0].percentage;
+                                return (<section key={tokenKey} className="FarmingTier">
+                                    <img src={window.formatLink(stakingData.rewardToken.logo || stakingData.rewardToken.logoURI)} />
+                                    <p>Earn 1 {stakingData.rewardToken.name} Card for every:</p>
+                                    <a className="GoToFarm" target="_blank" href={window.context.stakingUrlTemplate.format(stakingData.stakingManager.options.address)}>FARM</a>
+                                    <br></br>
+                                    <section className="FarmingTierDeal">
+                                        <img src={window.formatLink(stakingData.mainToken.logo || stakingData.mainToken.logoURI)} />
+                                        <p>{window.formatMoney(amountFor1, 2)} {stakingData.mainToken.symbol} locked in the Uniswap V2 pair {stakingData.mainToken.symbol} - {stakingData.pairs[0].symbol} {stakingData.tiers[0].tierKey} (Available: {window.fromDecimals(stakingData.tiers[0].remainingToStake, stakingData.mainToken.decimals)})</p>
+                                    </section>
+                                </section>);
+                            });
                         })}
                     </section>);
                 })}
