2f6191c0de180f14f9feba0626ca0f3eb12098d8
Marco Vasapollo <ceo@metaring.com>
2020-10-29 01:44:58 +0100

    Percentage

--- spa/farming/view.jsx
+++ spa/farming/view.jsx
@@ -38,7 +38,7 @@ var Farming = React.createClass({
                             var tokenValue = tokenEntry[1];
                             var stakingDatas = tokenValue.stakingData.stakingData;
                             return stakingDatas.map(stakingData => {
-                                var amountFor1 = stakingData.tiers[0].percentage;
+                                var amountFor1 = 100 / stakingData.tiers[0].percentage;
                                 return (<section key={tokenKey} className="FarmingTier">
                                     <img src={window.formatLink(stakingData.rewardToken.logo || stakingData.rewardToken.logoURI)} />
                                     <p>Earn 1 {stakingData.rewardToken.name} Card for every:</p>
