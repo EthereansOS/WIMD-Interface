d30ec557b4a0c9a6f0e95a5f7f9fb6694615d1cb
Marco Vasapollo <ceo@metaring.com>
2020-12-08 12:06:54 +0100

    OpenSea Link

--- spa/farming/view.jsx
+++ spa/farming/view.jsx
@@ -22,10 +22,10 @@ var Farming = React.createClass({
                 {this.props.sortedItems.map((it, i) => <section key={it.key} className="collection">
                     <CardInfo card={it} clean hideBalance />
                     {this.state && this.state.items && this.state.items[i] && <span className="UniPriceBro">$ {window.formatMoney(this.state.items[i].priceInDollars, 1)}</span>}
-                    <br></br>
+                    <br/>
                     <a className="unibtn" href={window.context.uniswapSpawUrlTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;swap</a>
                     <a className="unibtn" href={window.context.uniSwapInfoURL.format(it.erc20Wrapper)} target="_blank">&#129412;info</a>
-                    <a className="unibtn Opns" href={window.context.openSeaCollectionLinkTemplate.format(it.erc20Wrapper)} target="_blank">&#129412;opensea</a>
+                    <a className="unibtn Opns" href={window.context.openSeaItemLinkTemplate.format(window.whereIsMyDragonEthItem.options.address, it.key)} target="_blank">&#129412;opensea</a>
                 </section>)}
                 <section className="Delimitator"></section>
                 {(!this.state || !this.state.stakingContracts) && <InlineLoader />}
