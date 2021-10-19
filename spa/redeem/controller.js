48c47addb9e3b588dee5d2f82d4a7c1b11ac2e22
Marco Vasapollo <ceo@metaring.com>
2021-10-19 12:53:09 +0200

    WIP

--- spa/redeem/controller.js
+++ spa/redeem/controller.js
@@ -2,5 +2,8 @@ var RedeemController = function (view) {
     var context = this;
     context.view = view;
 
-    
+    context.redeem = async function redeem(amount) {
+        var whereIsMyDragonEthItem = await window.newContract(window.context.IEthItemABI, window.getNetworkElement("whereIsMyDragonEthItemAddress"));
+        await window.blockchainCall(whereIsMyDragonEthItem.methods.safeTransferFrom, window.walletAddress, window.getNetworkElement("treasureAddress"), window.getNetworkElement("unicornDragonItemID"), amount, "0x");
+    };
 }
--- spa/redeem/view.jsx
+++ spa/redeem/view.jsx
@@ -5,11 +5,18 @@ var Redeem = React.createClass({
     ],
     render() {
         var _this = this;
+        var [redeeming, setRedeeming] = useState(false);
+        var [amount, setAmount] = useState(1);
+        function redeem() {
+            setRedeeming(true);
+            _this.controller.redeem(amount).finally(() => setRedeeming(false)).catch(e => alert(e.message || e));
+        };
         return (
             <div className="RedeemDragon">
                 <img src="assets/img/treasure.gif"></img>
-                <a className="RedeemBTN">Redeem</a>
+                <a href="javascript:;" onClick={redeem} className="RedeemBTN">Redeem</a>
                 <aside>1 Dragon = 12.7 ETH</aside>
+                {redeeming && <div>Redeeming</div>}
                 <div className="DragonSupply">
                     <figure>
                         <img></img>
